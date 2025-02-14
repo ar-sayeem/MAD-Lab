a5d092560d7c2047dbfd65ad1968ab0480151f77
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2025-02-14 15:07:58 +0600

    first dart code

--- first.dart
+++ first.dart
@@ -1,3 +1,9 @@
-void main{
+import 'dart:io';
+
+void main() {
   print('My first dart code!');
+  stdout.write("Enter your name: ");
+
+  var name = stdin.readLineSync();
+  print("Welcome , $name. This are the basics of dart, keep learning!");
 }
