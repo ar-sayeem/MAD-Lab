1395c2c5473f37898d5f54a83a597914afa21bfe
Adnan Rahman Sayeem <98843020+ar-sayeem@users.noreply.github.com>
2025-02-17 23:49:26 +0600

    function_work.dart

--- function&work.dart
+++
@@ -1,8 +0,0 @@
-// function &  it's work
-
-void main(){
-
-
-
-
-}
---
+++ function_work.dart
@@ -0,0 +1,9 @@
+// function &  it's work
+
+void main() {
+  Greetings();
+}
+
+void Greetings() {
+  print("Hello everyone!, welcome to function class.");
+}
