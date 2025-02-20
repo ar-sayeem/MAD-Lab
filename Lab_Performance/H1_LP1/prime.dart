11f8093546866e1aa346d4e4459bbad244407be1
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2025-02-20 20:53:44 +0600

    done

--- Lab_Performance/H1_LP1/prime.dart
+++ Lab_Performance/H1_LP1/prime.dart
@@ -1,27 +1,27 @@
-String prime(int n) {
-  int flag = 0;
+// String prime(int n) {
+//   int flag = 0;
 
-  if (n == 0 || n == 1) {
-    flag = 1;
-  }
+//   if (n == 0 || n == 1) {
+//     flag = 1;
+//   }
 
-  for (int i = 2; i * i <= n; i++) {
-    if (n % i == 0) {
-      flag = 1;
-      break;
-    }
-  }
+//   for (int i = 2; i * i <= n; i++) {
+//     if (n % i == 0) {
+//       flag = 1;
+//       break;
+//     }
+//   }
 
-  if (flag == 0) {
-    return "Prime";
-  } else {
-    return "Not Prime";
-  }
-}
+//   if (flag == 0) {
+//     return "Prime";
+//   } else {
+//     return "Not Prime";
+//   }
+// }
 
-void main() {
-  print(prime(7));
-  print(prime(3));
-}
+// void main() {
+//   print(prime(7));
+//   print(prime(3));
+// }
 
 // Prime from a List
