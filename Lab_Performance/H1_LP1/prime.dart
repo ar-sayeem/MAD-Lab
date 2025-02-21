2c50320ce6cf7adc9b60f278de7fcec8d3e5f669
Adnan Rahman Sayeem <adnan.rahman.sayeem@mail.com>
2025-02-21 13:02:51 +0600

    Prime from a List

--- Lab_Performance/H1_LP1/prime.dart
+++ Lab_Performance/H1_LP1/prime.dart
@@ -1,3 +1,35 @@
+// Prime from a List
+
+bool isPrime(int n) {
+  if (n <= 1) return false;
+
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int countPrimes(List<int> l) {
+  List primeNumbers = [];
+
+  for (int i in l) {
+    if (isPrime(i)) {
+      primeNumbers.add(i);
+    }
+  }
+  print(
+      "Output: ${primeNumbers.length} (Prime numbers: ${primeNumbers.join(', ')})");
+  return primeNumbers.length;
+}
+
+void main() {
+  countPrimes([3, 8, 15, 20, 25, 7, 11]);
+}
+
+// Single Prime Number
+
 // String prime(int n) {
 //   int flag = 0;
 
@@ -23,5 +55,3 @@
 //   print(prime(7));
 //   print(prime(3));
 // }
-
-// Prime from a List
